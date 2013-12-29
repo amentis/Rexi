543978d6564eee1ec3584f4deaaa8fbdaa290ace
Amentis <ivan.g.bratoev@gmail.com>
2013-12-29 22:14:28 +0200

    Regular commit

--- index.py
+++ index.py
@@ -4,7 +4,8 @@ import sys
 
 sys.path.append("/home/amentis/Dropbox/Rexi")
 
-from UI import Console
+#from UI import Console
+from UI import Login
 
 sys.dont_write_bytecode = True
 
@@ -18,7 +19,8 @@ def application(environ, start_response):
     @return: return html request
     """
 
-    ui = Console.Console()
+    #ui = Console.Console()
+    ui = Login.Login()
     response_body = ui.get().encode('utf-8')
     status = '200 OK'
     response_headers = [('Content-Type', 'text/html; charset=utf-8'),
