c3f54ec9f837b268201a6e4eb523781082219c08
Amentis <ivan.g.bratoev@gmail.com>
2014-01-12 22:31:52 +0200

    Regular commit

--- UI/Login.py
+++ UI/Login.py
@@ -15,6 +15,6 @@ class Login(Screen):
         self.__subtitle = Label(self.__window, "Remote-accessed Environment-independent eXecuting Interface")
         self.__login_form = Window(self.__window, "Login form")
         self.__username_label = Label(self.__login_form, "Username: ")
-        self.__username_field = LineEdit(self.__login_form, "username")
+        self.__username_field = LineEdit(self.__login_form, "username", "username")
         self.__password_label = Label(self.__login_form, "Password: ")
-        self.__password_field = PasswordEdit(self.__login_form, "password")
+        self.__password_field = PasswordEdit(self.__login_form, "password", "password")
