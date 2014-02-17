57473c851905f605777ced4c6bae7a8f069f1ff7
Amentis <ivan.g.bratoev@gmail.com>
2014-02-17 12:45:16 +0200

    Added Beaker

--- index.py
+++ index.py
@@ -4,21 +4,26 @@ import sys
 
 sys.path.append("/home/amentis/Dropbox/Rexi")
 
-#from UI import Console
+from beaker.middleware import SessionMiddleware
+from UI import Console
 from UI import Login
 
 
 #noinspection PyUnusedLocal
-def application(environ, start_response):
+def starter(environ, start_response):
     """
-
     @param environ: standard environ
     @param start_response: standard start_response
     @return: return html request
     """
 
-    #ui = Console.Console()
-    ui = Login.Login()
+    session = environ['beaker.session']
+
+    if 'logged_in' in session:  # check in database if last attempt is a login and successful
+        ui = Console.Console()
+    else:
+        ui = Login.Login()
+
     response_body = ui.get().encode('utf-8')
     status = '200 OK'
     response_headers = [('Content-Type', 'text/html; charset=utf-8'),
@@ -26,3 +31,11 @@ def application(environ, start_response):
     start_response(status, response_headers)
 
     return [response_body]
+
+session_opts = {
+    'session_type': 'file',
+    'session_cookie_expires': False,
+}
+
+
+application = SessionMiddleware(starter, session_opts)
